0b2433bda3becfb0633b5923edc3c132a516bcec
Ric Featherstone <ric@control-plane.io>
2023-04-17 16:34:16 +0100

    tidy jwks retriever

--- jwks-retriever/main.go
+++ jwks-retriever/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"github.com/spiffe/go-spiffe/v2/bundle/jwtbundle"
 	spiffev2 "github.com/spiffe/go-spiffe/v2/spiffeid"
 	apiv2 "github.com/spiffe/go-spiffe/v2/workloadapi"
 	"log"
@@ -10,39 +11,78 @@ import (
 	"syscall"
 )
 
+var (
+	trustDomain = spiffev2.RequireTrustDomainFromString(os.Getenv("TRUST_DOMAIN"))
+	jwksPath    = os.Getenv("JWKS_PATH")
+)
+
 func main() {
 	ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
 	defer stop()
 
-	jwtSource, err := apiv2.NewJWTSource(ctx)
+	bundleHelper, err := NewJwtHelper(ctx, trustDomain)
 	if err != nil {
 		stop()
-		log.Fatal("unable to create jwtSource:", err)
+		log.Fatalf("failed to create bundle helper: %v", err)
 	}
-	defer jwtSource.Close()
+	defer func(bundleHelper BundleHelper) {
+		_ = bundleHelper.Close()
+	}(bundleHelper)
 
-	trustDomain := spiffev2.RequireTrustDomainFromString(os.Getenv("TRUST_DOMAIN"))
-	jwksPath := os.Getenv("JWKS_PATH")
+	bundleStream := make(chan *jwtbundle.Bundle, 1)
 
-	writeBundle(jwtSource, trustDomain, jwksPath)
+	bundle, err := bundleHelper.GetBundle(ctx)
+	if err != nil {
+		stop()
+		log.Fatalf("failed to get bundle: %v", err)
+	}
 
-	<-ctx.Done()
-	log.Println("shutting down")
+	bundleStream <- bundle
+
+	// TODO: Watch for bundle updates and upload to bucket
+	go func() {
+		for {
+			select {
+			case <-ctx.Done():
+				stop()
+				log.Println("shutting down")
+				os.Exit(0)
+			}
+		}
+	}()
+
+	for bundle := range bundleStream {
+		bytes, err := bundle.Marshal()
+		if err != nil {
+			log.Fatal("unable to marshall bundle:", err)
+		}
+
+		err = os.WriteFile(jwksPath, bytes, 0644)
+		if err != nil {
+			log.Fatal("unable to write bundle", err)
+		}
+	}
 }
 
-func writeBundle(source *apiv2.JWTSource, trustDomain spiffev2.TrustDomain, jwksPath string) {
-	bundle, err := source.GetJWTBundleForTrustDomain(trustDomain)
+func NewJwtHelper(ctx context.Context, trustDomain spiffev2.TrustDomain) (helper BundleHelper, err error) {
+	jwtSource, err := apiv2.NewJWTSource(ctx)
 	if err != nil {
-		log.Fatal("unable to fetch bundle:", err)
+		return
 	}
 
-	bytes, err := bundle.Marshal()
-	if err != nil {
-		log.Fatal("unable to marshall bundle:", err)
-	}
+	helper = BundleHelper{jwtSource, trustDomain}
+	return
+}
 
-	err = os.WriteFile(jwksPath, bytes, 0644)
-	if err != nil {
-		log.Fatal("unable to write bundle", err)
-	}
+type BundleHelper struct {
+	source      *apiv2.JWTSource
+	trustDomain spiffev2.TrustDomain
+}
+
+func (h BundleHelper) GetBundle(ctx context.Context) (bundle *jwtbundle.Bundle, err error) {
+	return h.source.GetJWTBundleForTrustDomain(h.trustDomain)
+}
+
+func (h BundleHelper) Close() error {
+	return h.source.Close()
 }
