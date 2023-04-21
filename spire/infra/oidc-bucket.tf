d5443f2c38c1c22bed56fb9048c3e47372657bfe
Ric Featherstone <ric@control-plane.io>
2023-04-21 19:02:10 +0100

    fix: issues creating public oidc bucket
    https://aws.amazon.com/about-aws/whats-new/2022/12/amazon-s3-automatically-enable-block-public-access-disable-access-control-lists-buckets-april-2023/

--- spire/infra/oidc-bucket.tf
+++ spire/infra/oidc-bucket.tf
@@ -4,7 +4,27 @@ resource "aws_s3_bucket" "oidc" {
   force_destroy = true
 }
 
+resource "aws_s3_bucket_ownership_controls" "oidc" {
+  bucket = aws_s3_bucket.oidc.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "oidc" {
+  bucket = aws_s3_bucket.oidc.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
 resource "aws_s3_bucket_acl" "oidc" {
   bucket = aws_s3_bucket.oidc.id
   acl    = "public-read"
+
+  depends_on = [
+    aws_s3_bucket_public_access_block.oidc,
+  ]
 }
